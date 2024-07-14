597aa448bf2d2a9b93c45d88aa8b183ae271cd09
Vova <redmindblow@gmail.com>
2024-07-14 15:08:22 +0300

    HW-04

---
+++ second_task.py
@@ -0,0 +1,16 @@
+from os import path as file_path
+
+def get_cats_info(path: str) -> list[dict]:
+    """
+    Read file and transform data in it to list of dictionaries
+
+    Returns:
+        List of dicts 
+    """
+    if file_path.exists(path):
+        with open(path, 'r', encoding='utf-8') as fh:
+            lines = fh.readlines()
+        parsed_lines = [line.strip().split(',') for line in lines]
+        dict_keys = ['id', 'name', 'age']
+        return [dict(zip(dict_keys,vals)) for vals in parsed_lines]
+    return 'File not exist'
